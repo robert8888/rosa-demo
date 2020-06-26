93f419bac182240077d2fde82c8ea083e449fef2
unknown <robert.kami88@gmail.com>
2020-06-26 12:55:45 +0200

    Updates

---
+++ precache-manifest.5bc5da77505af0ac88cde964c7913c23.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bf2d87908ad85b4241901ed6bfacc9c",
+    "url": "/robert8888/rosa-demo.git/index.html"
+  },
+  {
+    "revision": "c2fa3d7f57f8e6332555",
+    "url": "/robert8888/rosa-demo.git/static/css/2.e70e3282.chunk.css"
+  },
+  {
+    "revision": "abbb824163ece09218f3",
+    "url": "/robert8888/rosa-demo.git/static/css/main.e4f66e19.chunk.css"
+  },
+  {
+    "revision": "c2fa3d7f57f8e6332555",
+    "url": "/robert8888/rosa-demo.git/static/js/2.ac28f429.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robert8888/rosa-demo.git/static/js/2.ac28f429.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abbb824163ece09218f3",
+    "url": "/robert8888/rosa-demo.git/static/js/main.0e95dd1c.chunk.js"
+  },
+  {
+    "revision": "547c1f502b6c2c5f6bde",
+    "url": "/robert8888/rosa-demo.git/static/js/runtime-main.d13352e0.js"
+  }
+]);
