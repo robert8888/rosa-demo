cf121f99cf39f0b73c7a24f04df4d865215b7758
unknown <robert.kami88@gmail.com>
2020-06-26 13:03:25 +0200

    Rosa demo page V1

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+import Header from "./components/Header"
+import Sections from "./components/Sections/Sections";
+import Footer from "./components/Footer";
+
+const App = () => (
+    <>
+    <Header/>
+    <Sections/>
+    <Footer/>
+    </>
+);
+
+export default App;
